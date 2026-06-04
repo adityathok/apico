7039e86aeda345af2a544a40fe921f994829b6d8
adityathok <aditya0thok@gmail.com>
2026-06-04 09:30:01 +0700

    feat; ubah route admin

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,12 @@ Route::inertia('/', 'Welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
-    Route::inertia('post', 'Post')->name('post');
-    Route::inertia('posts', 'Posts')->name('posts');
-    Route::inertia('categories', 'Categories')->name('categories');
-    Route::inertia('tags', 'Tags')->name('tags');
-    Route::inertia('licenses', 'Licenses')->name('licenses');
-    Route::inertia('websites', 'Websites')->name('websites');
+    Route::inertia('admin/post', 'Post')->name('post');
+    Route::inertia('admin/posts', 'Posts')->name('posts');
+    Route::inertia('admin/categories', 'Categories')->name('categories');
+    Route::inertia('admin/tags', 'Tags')->name('tags');
+    Route::inertia('admin/licenses', 'Licenses')->name('licenses');
+    Route::inertia('admin/websites', 'Websites')->name('websites');
 });
 
 require __DIR__ . '/settings.php';
